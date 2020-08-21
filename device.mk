83ee5578a15dc50599f713985ce6361fad8cf6da
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    RMX1801: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- device.mk
+++ device.mk
@@ -442,7 +442,8 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf \
-    WifiOverlay
+    WifiOverlay \
+    TetheringConfigOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
