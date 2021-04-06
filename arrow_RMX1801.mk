dadd26f0c3c0542afc8f4edab698ac4eeff1daa0
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-04-06 14:11:18 +0530

    RMX1801: update redfin fp April

--- arrow_RMX1801.mk
+++ arrow_RMX1801.mk
@@ -43,4 +43,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1801" \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1.191014.001 eng.root.20200727.144331 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
