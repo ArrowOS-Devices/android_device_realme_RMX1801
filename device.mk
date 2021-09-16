1c0ba506a1d16e17d2b3480fe67535418a10ca9f
EcrosoftXiao <xy19990903@gmail.com>
2021-09-16 14:34:30 +0100

    RMX1801: Switch to Thermal 2.0 mock
    * QTI Thermal couldn't read the temperature for some reason
    * sepolicy: Adjust sepolicy for thermal

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.mock
 
 # USB
 PRODUCT_PACKAGES += \
