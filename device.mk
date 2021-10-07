b3e1a8617f1fc460a4cc0ff36344ab82fb7dbf84
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-10-07 15:09:00 +0000

    RMX1801: Add atrace-hal build rule
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
 
+
+# ATRACE_HAL
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
@@ -153,7 +158,6 @@ PRODUCT_PACKAGES += \
     libbluetooth_audio_session \
     libbthost_if
 
-# Bluetooth 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
