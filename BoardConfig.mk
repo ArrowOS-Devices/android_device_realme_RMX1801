24f88c89ea0301a6af8c063cc7df44c0eb73fa26
SakilMondal <mondalsakil@gmail.com>
2021-10-24 06:07:53 +0000

    RMX1801: Update sepolicy flags for android 12
    Change-Id: I1b7dc0ea48394245f4b72872315211bc2a986966
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor init
