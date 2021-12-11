12cff6d415b6fc89e9c6a3f7430c4c11c24ce1cb
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-12-11 10:12:45 +0700

    RMX1801: Import Oppo Health Service and label it
    * In order to fix battery stuck at 50% after boot for a moment
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -247,8 +247,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
