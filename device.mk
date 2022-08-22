ebc2ef9f3482f212b9eddad358829c5b520fa7e0
karthik1896 <karthik1896@outlook.com>
2022-08-22 07:37:17 +0000

    RMX1801: Remove audio A2dp

--- device.mk
+++ device.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudiopreprocessing \
