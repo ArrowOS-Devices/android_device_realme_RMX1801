5151b92ce47fb92bf635f497027e4d73dc4c79f2
johnmart19 <johnivan19999@gmail.com>
2022-09-07 21:54:13 +0000

    RMX1801:Build missing Soundtrigger 2.0
     * W libc : Unable to set property
     "ctl.interface_start" to
     "android.hardware.soundtrigger@2.0::ISoundTriggerHw/default":
     error code: 0x20

--- device.mk
+++ device.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.r_submix.default \
     audio.usb.default \
